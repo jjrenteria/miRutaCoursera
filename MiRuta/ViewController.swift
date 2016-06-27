4bb0b54b12dcad075406348b28bfcaf4697b0c6f
Juan Jose Renteria <juanjoserenteria@macbook-pro-de-juan.local>
2016-06-27 10:37:24 -0500

    Initial Commit

---
+++ MiRuta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiRuta
+//
+//  Created by Juan Jose Renteria on 27/06/16.
+//  Copyright © 2016 Juan Jose Renteria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
